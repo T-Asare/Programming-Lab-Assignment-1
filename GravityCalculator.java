69a7981fc6e757a570f13cb6383094cc7f0875dc
T-Asare <77137510+T-Asare@users.noreply.github.com>
2021-01-17 01:18:54 +0000

    Update GravityCalculator.java

--- GravityCalculator.java
+++ GravityCalculator.java
@@ -1,5 +1,5 @@
 public class GravityCalculator {
-            public static void main(String[] arguments) {
+           // public static void main(String[] arguments) {
             double gravity = -9.81; // Earth's gravity in m/s^2
             double initialVelocity = 0.0;
             double fallingTime = 10.0;
@@ -11,5 +11,5 @@ public class GravityCalculator {
 
                 //java: class GravityCalculator is already defined in package unnamed package(Initial response)
             //The object's position after 10.0 seconds is 0.0 m. (Response when one of the class statements was deleted)
-        }
+       // }
     }
